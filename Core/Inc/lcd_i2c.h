c64244c0b29f2bd0ff82bb7d8321b4d4db1c3339
wuyshng <quy.maximus.39@gmail.com>
2024-05-19 12:29:08 +0700

    fix LCD flickering

--- Core/Inc/lcd_i2c.h
+++ Core/Inc/lcd_i2c.h
@@ -16,21 +16,24 @@ extern I2C_HandleTypeDef hi2c1;
 extern int32_t curEncPos;
 extern int32_t oldEncPos;
 extern uint32_t stateMenu;
+extern uint8_t oldStateMenu;
 extern int32_t menuMainCnt;
 extern int32_t menuMainLength;
 extern int32_t optionSelect;
 extern int32_t optionMenuLength;
 
-void LCD_Init(void);					// initialize lcd
-void LCD_Send_Cmd(char cmd);			// send command to the lcd
-void LCD_Send_Data(char data);			// send data to the lcd
-void LCD_Send_String(char *str);		// send string to the lcd
-void LCD_Clear_Display(void);			// clear display lcd
-void LCD_Goto_XY(int row, int col);		// set proper location on screen
-void LCD_Clear_Line(int line); 			// clear line
+void LCD_Init(void);					        // initialize lcd
+void LCD_Send_Cmd(char cmd);			        // send command to the lcd
+void LCD_Send_Data(char data);			        // send data to the lcd
+void LCD_Send_String(char *str);		        // send string to the lcd
+void LCD_Clear_Display(void);			        // clear display lcd
+void LCD_Goto_XY(int row, int col);		        // set proper location on screen
+void LCD_Clear_Line(int rowNum);				// clear line
+void LCD_Clear_Pos(int rowNum, int colNum);		// clear at position
 void loop_menu(void);
 void update_menu(int menuSelect);
 void setting_value(int menuSelect);
 void update_setting_value(int menuSelect);
+void push_button_menu(void);
 
 #endif /* INC_LCD_I2C_H_ */
