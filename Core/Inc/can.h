8c115e36ffdf3b1bb945a64877ef3bfc5aab8457
wuyshng <quy.maximus.39@gmail.com>
2024-05-19 13:49:41 +0700

    fix can

--- Core/Inc/can.h
+++ Core/Inc/can.h
@@ -1,84 +1,84 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    can.h
-  * @brief   This file contains all the function prototypes for
-  *          the can.c file
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    can.h
+ * @brief   This file contains all the function prototypes for
+ *          the can.c file
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __CAN_H__
 #define __CAN_H__
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* USER CODE BEGIN Includes */
-#include "stdint.h"
-/* USER CODE END Includes */
+    /* USER CODE BEGIN Includes */
 
-extern CAN_HandleTypeDef hcan;
+    /* USER CODE END Includes */
 
-/* USER CODE BEGIN Private defines */
-extern CAN_TxHeaderTypeDef TxHeader;
-extern CAN_RxHeaderTypeDef RxHeader;
+    extern CAN_HandleTypeDef hcan;
 
-extern int DataCheck;
-extern int obd2_check;
+    /* USER CODE BEGIN Private defines */
+    extern CAN_TxHeaderTypeDef TxHeader;
+    extern CAN_RxHeaderTypeDef RxHeader;
 
-typedef struct CAN_message_t {
-    uint32_t id;          // can identifier
-    uint16_t timestamp;   // FlexCAN time when message arrived
-    struct {
-        uint8_t extended:1; // identifier is extended (29-bit)
-        uint8_t remote:1;   // remote transmission request packet type
-        uint8_t overrun:1;  // message overrun
-        uint8_t reserved:5;
-    } flags;
-    uint8_t len;          // length of data
-    uint8_t buf[8];
-} CAN_message_t;
+    extern int DataCheck;
+    extern int obd2_check;
 
-enum CANbaudRatePrescaler{
-	CAN_1000kbit = 2,
-	CAN_500kbit = 4,
-	CAN_250kbit = 8,
-	CAN_125kbit = 16,
-	CAN_100kbit = 20
-};
-/* USER CODE END Private defines */
+    typedef struct CAN_message_t
+    {
+        uint32_t id;        // can identifier
+        uint16_t timestamp; // FlexCAN time when message arrived
+        struct
+        {
+            uint8_t extended : 1; // identifier is extended (29-bit)
+            uint8_t remote : 1;   // remote transmission request packet type
+            uint8_t overrun : 1;  // message overrun
+            uint8_t reserved : 5;
+        } flags;
+        uint8_t len; // length of data
+        uint8_t buf[8];
+    } CAN_message_t;
 
-void MX_CAN_Init(void);
+    enum CANbaudRatePrescaler
+    {
+        CAN_1000kbit = 2,
+        CAN_500kbit = 4,
+        CAN_250kbit = 8,
+        CAN_125kbit = 16,
+        CAN_100kbit = 20
+    };
+    /* USER CODE END Private defines */
 
-/* USER CODE BEGIN Prototypes */
-//void CAN_MsgTx(void);
-//void CAN_MsgTx2(void);
-//void Send_Response(void);
-void CAN_SetBaudRate(uint32_t baudrate);
-void CAN_FilterConfig(void);
-void CAN_Tx(void);
-void CAN_Tx2(void);
-void Send_Response(void);
-/* USER CODE END Prototypes */
+    void MX_CAN_Init(void);
+
+    /* USER CODE BEGIN Prototypes */
+    void CAN_SetBaudRate(uint32_t baudrate);
+    void CAN_FilterConfig(void);
+    void CAN_Tx(void);
+    void CAN_Tx2(void);
+    void Send_Response(void);
+    /* USER CODE END Prototypes */
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __CAN_H__ */
-
--- Core/Src/can.c
+++ Core/Src/can.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    can.c
-  * @brief   This file provides code for the configuration
-  *          of the CAN instances.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    can.c
+ * @brief   This file provides code for the configuration
+ *          of the CAN instances.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "can.h"
@@ -36,220 +36,210 @@ CAN_HandleTypeDef hcan;
 void MX_CAN_Init(void)
 {
 
-  /* USER CODE BEGIN CAN_Init 0 */
+    /* USER CODE BEGIN CAN_Init 0 */
 
-  /* USER CODE END CAN_Init 0 */
+    /* USER CODE END CAN_Init 0 */
 
-  /* USER CODE BEGIN CAN_Init 1 */
+    /* USER CODE BEGIN CAN_Init 1 */
 
-  /* USER CODE END CAN_Init 1 */
-  hcan.Instance = CAN1;
-  hcan.Init.Prescaler = 4;
-  hcan.Init.Mode = CAN_MODE_NORMAL;
-  hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan.Init.TimeSeg1 = CAN_BS1_15TQ;
-  hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
-  hcan.Init.TimeTriggeredMode = DISABLE;
-  hcan.Init.AutoBusOff = DISABLE;
-  hcan.Init.AutoWakeUp = DISABLE;
-  hcan.Init.AutoRetransmission = DISABLE;
-  hcan.Init.ReceiveFifoLocked = DISABLE;
-  hcan.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN CAN_Init 2 */
+    /* USER CODE END CAN_Init 1 */
+    hcan.Instance = CAN1;
+    hcan.Init.Prescaler = 4;
+    hcan.Init.Mode = CAN_MODE_NORMAL;
+    hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
+    hcan.Init.TimeSeg1 = CAN_BS1_15TQ;
+    hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
+    hcan.Init.TimeTriggeredMode = DISABLE;
+    hcan.Init.AutoBusOff = DISABLE;
+    hcan.Init.AutoWakeUp = DISABLE;
+    hcan.Init.AutoRetransmission = DISABLE;
+    hcan.Init.ReceiveFifoLocked = DISABLE;
+    hcan.Init.TransmitFifoPriority = DISABLE;
+    if (HAL_CAN_Init(&hcan) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN CAN_Init 2 */
 
-  /* USER CODE END CAN_Init 2 */
-
+    /* USER CODE END CAN_Init 2 */
 }
 
-void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
+void HAL_CAN_MspInit(CAN_HandleTypeDef *canHandle)
 {
 
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(canHandle->Instance==CAN1)
-  {
-  /* USER CODE BEGIN CAN1_MspInit 0 */
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if (canHandle->Instance == CAN1)
+    {
+        /* USER CODE BEGIN CAN1_MspInit 0 */
 
-  /* USER CODE END CAN1_MspInit 0 */
-    /* CAN1 clock enable */
-    __HAL_RCC_CAN1_CLK_ENABLE();
+        /* USER CODE END CAN1_MspInit 0 */
+        /* CAN1 clock enable */
+        __HAL_RCC_CAN1_CLK_ENABLE();
 
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**CAN GPIO Configuration
-    PA11     ------> CAN_RX
-    PA12     ------> CAN_TX
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_11;
-    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        /**CAN GPIO Configuration
+        PA11     ------> CAN_RX
+        PA12     ------> CAN_TX
+        */
+        GPIO_InitStruct.Pin = GPIO_PIN_11;
+        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = GPIO_PIN_12;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+        GPIO_InitStruct.Pin = GPIO_PIN_12;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    /* CAN1 interrupt Init */
-    HAL_NVIC_SetPriority(USB_LP_CAN1_RX0_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(USB_LP_CAN1_RX0_IRQn);
-  /* USER CODE BEGIN CAN1_MspInit 1 */
+        /* CAN1 interrupt Init */
+        HAL_NVIC_SetPriority(USB_LP_CAN1_RX0_IRQn, 0, 0);
+        HAL_NVIC_EnableIRQ(USB_LP_CAN1_RX0_IRQn);
+        /* USER CODE BEGIN CAN1_MspInit 1 */
 
-  /* USER CODE END CAN1_MspInit 1 */
-  }
+        /* USER CODE END CAN1_MspInit 1 */
+    }
 }
 
-void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
+void HAL_CAN_MspDeInit(CAN_HandleTypeDef *canHandle)
 {
 
-  if(canHandle->Instance==CAN1)
-  {
-  /* USER CODE BEGIN CAN1_MspDeInit 0 */
+    if (canHandle->Instance == CAN1)
+    {
+        /* USER CODE BEGIN CAN1_MspDeInit 0 */
 
-  /* USER CODE END CAN1_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_CAN1_CLK_DISABLE();
+        /* USER CODE END CAN1_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_CAN1_CLK_DISABLE();
 
-    /**CAN GPIO Configuration
-    PA11     ------> CAN_RX
-    PA12     ------> CAN_TX
-    */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
+        /**CAN GPIO Configuration
+        PA11     ------> CAN_RX
+        PA12     ------> CAN_TX
+        */
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11 | GPIO_PIN_12);
 
-    /* CAN1 interrupt Deinit */
-    HAL_NVIC_DisableIRQ(USB_LP_CAN1_RX0_IRQn);
-  /* USER CODE BEGIN CAN1_MspDeInit 1 */
+        /* CAN1 interrupt Deinit */
+        HAL_NVIC_DisableIRQ(USB_LP_CAN1_RX0_IRQn);
+        /* USER CODE BEGIN CAN1_MspDeInit 1 */
 
-  /* USER CODE END CAN1_MspDeInit 1 */
-  }
+        /* USER CODE END CAN1_MspDeInit 1 */
+    }
 }
 
 /* USER CODE BEGIN 1 */
 void CAN_SetBaudRate(uint32_t baudrate)
 {
-	/* Usually the APB1 clock on STM32F103 is running at max clock frequencies 36 MHz */
-	//uint32_t APB1clockSpeed = HAL_RCC_GetPCLK1Freq();
-	uint32_t prescaler = 0;
+    /* Usually the APB1 clock on STM32F103 is running at max clock frequencies 36 MHz */
+    // uint32_t APB1clockSpeed = HAL_RCC_GetPCLK1Freq();
+    uint32_t prescaler = 0;
 
-    switch (baudrate) {
-        case 1000000:
-            prescaler = CAN_1000kbit;
-            break;
-        case 500000:
-            prescaler = CAN_500kbit;
-            break;
-        case 250000:
-            prescaler = CAN_250kbit;
-            break;
-        case 125000:
-            prescaler = CAN_125kbit;
-            break;
-        case 100000:
-            prescaler = CAN_100kbit;
-            break;
+    switch (baudrate)
+    {
+    case 1000000:
+        prescaler = CAN_1000kbit;
+        break;
+    case 500000:
+        prescaler = CAN_500kbit;
+        break;
+    case 250000:
+        prescaler = CAN_250kbit;
+        break;
+    case 125000:
+        prescaler = CAN_125kbit;
+        break;
+    case 100000:
+        prescaler = CAN_100kbit;
+        break;
     }
     hcan.Init.Prescaler = prescaler;
-	hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
-	hcan.Init.TimeSeg1 = CAN_BS1_15TQ;
-	hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
+    hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
+    hcan.Init.TimeSeg1 = CAN_BS1_15TQ;
+    hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
 }
 
 void CAN_FilterConfig(void)
 {
-  CAN_FilterTypeDef canfilterconfig;
+    CAN_FilterTypeDef canfilterconfig;
 
-  canfilterconfig.FilterActivation = CAN_FILTER_ENABLE;
-  canfilterconfig.FilterBank = 10;  	// which filter bank to use from the assigned ones
-  canfilterconfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
-  canfilterconfig.FilterIdHigh = 0x0000;
-  canfilterconfig.FilterIdLow = 0x0000;
-  canfilterconfig.FilterMaskIdHigh = 0x0000;
-  canfilterconfig.FilterMaskIdLow = 0x0000;
-  canfilterconfig.FilterMode = CAN_FILTERMODE_IDMASK;
-  canfilterconfig.FilterScale = CAN_FILTERSCALE_32BIT;
-  canfilterconfig.SlaveStartFilterBank = 0;
+    canfilterconfig.FilterActivation = CAN_FILTER_ENABLE;
+    canfilterconfig.FilterBank = 10; // which filter bank to use from the assigned ones
+    canfilterconfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+    canfilterconfig.FilterIdHigh = 0x0000;
+    canfilterconfig.FilterIdLow = 0x0000;
+    canfilterconfig.FilterMaskIdHigh = 0x0000;
+    canfilterconfig.FilterMaskIdLow = 0x0000;
+    canfilterconfig.FilterMode = CAN_FILTERMODE_IDMASK;
+    canfilterconfig.FilterScale = CAN_FILTERSCALE_32BIT;
+    canfilterconfig.SlaveStartFilterBank = 0;
 
-  HAL_CAN_ConfigFilter(&hcan, &canfilterconfig);
+    HAL_CAN_ConfigFilter(&hcan, &canfilterconfig);
 }
 
 void CAN_Tx(void)
 {
-	TxHeader.StdId = 0x111;		/* ID message */
-	TxHeader.IDE = CAN_ID_STD;
-	TxHeader.RTR = CAN_RTR_DATA;
-	TxHeader.DLC = 8;			/* Data length */
+    TxHeader.StdId = 0x111; /* ID message */
+    TxHeader.IDE = CAN_ID_STD;
+    TxHeader.RTR = CAN_RTR_DATA;
+    TxHeader.DLC = 8; /* Data length */
 
-	TxData[0] = 0xFF;
-	TxData[1] = 0x0A;
-	TxData[2] = 0x15;
-	TxData[3] = 0x33;
-	TxData[4] = 0x45;
-	TxData[5] = 0x29;
-	TxData[6] = 0x11;
-	TxData[7] = 0x00;
-	HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox);	//request transmission of a new message
+    TxData[0] = 0xFF;
+    TxData[1] = 0x0A;
+    TxData[2] = 0x15;
+    TxData[3] = 0x33;
+    TxData[4] = 0x45;
+    TxData[5] = 0x29;
+    TxData[6] = 0x11;
+    TxData[7] = 0x00;
+    HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox); // request transmission of a new message
 }
 
 void CAN_Tx2(void)
 {
-	TxHeader.StdId = 0x119;		/* ID message */
-	TxHeader.IDE = CAN_ID_STD;
-	TxHeader.RTR = CAN_RTR_DATA;
-	TxHeader.DLC = 8;			/* Data length */
+    TxHeader.StdId = 0x119; /* ID message */
+    TxHeader.IDE = CAN_ID_STD;
+    TxHeader.RTR = CAN_RTR_DATA;
+    TxHeader.DLC = 8; /* Data length */
 
-	TxData[0] = 0xAA;
-	TxData[1] = 0x01;
-	TxData[2] = 0x47;
-	TxData[3] = 0x89;
-	TxData[4] = 0xB1;
-	TxData[5] = 0xB5;
-	TxData[6] = 0xE7;
-	TxData[7] = 0x01;
-	HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox);	//request transmission of a new message
+    TxData[0] = 0xAA;
+    TxData[1] = 0x01;
+    TxData[2] = 0x47;
+    TxData[3] = 0x89;
+    TxData[4] = 0xB1;
+    TxData[5] = 0xB5;
+    TxData[6] = 0xE7;
+    TxData[7] = 0x01;
+    HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox); // request transmission of a new message
 }
 
 void Send_Response(void)
 {
-	TxHeader.StdId = 0x112;		/* ID message */
-	TxHeader.IDE = CAN_ID_STD;
-	TxHeader.RTR = CAN_RTR_DATA;
-	TxHeader.DLC = 8;			/* Data length */
+    TxHeader.StdId = 0x112; /* ID message */
+    TxHeader.IDE = CAN_ID_STD;
+    TxHeader.RTR = CAN_RTR_DATA;
+    TxHeader.DLC = 8; /* Data length */
 
-	TxData[0] = 0xF3;
-	TxData[1] = 0x52;
-	TxData[2] = 0x10;
-	TxData[3] = 0x33;
-	TxData[4] = 0x45;
-	TxData[5] = 0x29;
-	TxData[6] = 0x11;
-	TxData[7] = 0x00;
-	HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox);	//request transmission of a new message
+    TxData[0] = 0xF3;
+    TxData[1] = 0x52;
+    TxData[2] = 0x10;
+    TxData[3] = 0x33;
+    TxData[4] = 0x45;
+    TxData[5] = 0x29;
+    TxData[6] = 0x11;
+    TxData[7] = 0x00;
+    HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox); // request transmission of a new message
 }
 
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
 {
-	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-	HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData); //retrieved message in FIFO
-//	if(RxHeader.RTR == CAN_RTR_DATA)
-//	{
-//		DataCheck = 1;
-//	}
-//	else if(RxHeader.StdId == 0x200 && RxHeader.RTR == CAN_RTR_REMOTE)
-//	{
-//		Send_Response();
-//	}
-	if(RxHeader.StdId == 0x7DF)
-	{
-		ecu_send_respond();
-		//obd2_check = 1;
-	}
-
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData); // retrieved message in FIFO
+    if (RxHeader.StdId == 0x7DF)
+    {
+        ecu_send_respond();
+    }
 }
 
 void HAL_CAN_TxMailbox0CompleteCallback(CAN_HandleTypeDef *hcan)
 {
-	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
 }
 /* USER CODE END 1 */
